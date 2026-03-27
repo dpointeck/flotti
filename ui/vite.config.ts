2c15b37fb98e1789ee6af34577d1ecb3612efeec
dpointeck <daniel.pointecker@gmail.com>
2026-03-27 09:13:42 +0100

    init

---
+++ ui/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite-plus";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+});
