bc798a95ba65287cafe346c335c62fbd5ff53f82
dpointeck <daniel.pointecker@gmail.com>
2026-04-21 09:04:59 +0200

    setup ui

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite-plus";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   lint: { options: { typeAware: true, typeCheck: true } },
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+          target: 'react',
+          autoCodeSplitting: true,
+        }),react(), tailwindcss()],
 });
